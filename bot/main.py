f31c2a74a5bcb26ad25f21d321f973e38439f854
daysguin-ship-it <daysguin@gmail.com>
2025-12-27 20:49:52 -0500

    Add main bot file

---
+++ bot/main.py
@@ -0,0 +1,18 @@
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    await bot.tree.sync()
+    print(f"Logged in as {bot.user}")
+
+bot.run(os.getenv("DISCORD_TOKEN"))
